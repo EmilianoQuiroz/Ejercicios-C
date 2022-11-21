ae16918577d93acb26592843a3e587ddc01c7697
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-21 19:06:30 -0300

    Ejercicio calcular area de un trapecio

---
+++ 05-area_de_trapecio.c
@@ -0,0 +1,28 @@
+//Area de un trapecio
+/*Pedirle al usuario las medidas necesarias para hacer el calculo del area del trapecio y mostrar el resultado en pantalla*/
+#include<stdio.h>
+
+int main()
+{
+    /*1.Iniciamos la variables que necesitaremos para hacer el calculo*/
+    float baseMenor,baseMayor,altura,areaDelTrapecio;
+
+    /*2.Pedimos al usuario que ingrese los datos necesarios*/
+    printf("Ingrese la medida de la base menor \n");
+    /*3.Ingresamos los datos en las variables correspondientes*/
+    scanf("%f", &baseMenor);
+    //Repetimos este proceso para cada dato que necesitemos
+    printf("Ingrese la medida de la base mayor \n");
+    scanf("%f", &baseMayor);
+
+    printf("Ingrese la medida de la altura \n");
+    scanf("%f", &altura);
+
+    /*4.Hacemos el calculo del area del trapecio*/
+    areaDelTrapecio = ((baseMayor + baseMenor) * altura) / 2;
+
+    /*5.Mostramos en pantalla el resultado*/
+    printf("El area del trapecio es: %.2f",areaDelTrapecio);
+
+    return 0; 
+}
