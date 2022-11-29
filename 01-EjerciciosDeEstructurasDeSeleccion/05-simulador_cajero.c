8e90ebad78e67c60db4ded49e2fb8591912d09f8
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-29 20:46:54 -0300

    Ejercicio simulador de cajero automatico

--- 01-EjerciciosDeEstructurasDeSeleccion/05-simulador_cajero.c
+++ 01-EjerciciosDeEstructurasDeSeleccion/05-simulador_cajero.c
@@ -18,9 +18,9 @@ int main()
     printf("\n1)Depositar Dinero");
     printf("\n2)Retirar Dinero");
     printf("\n3)Salir");
-    printf("\nElija una opcion");
+    printf("\nElija una opcion\n");
 
-    /*3.Guardamos la opcion elejida en la variable opcion*/
+    /*3.Guardamos la opcion elegida en la variable opcion*/
     scanf("%i", &opcion);
 
     /*4.Creamos el condicional switch*/
@@ -36,6 +36,33 @@ int main()
         /*4.Mostramos en pantalla el monto inicial mas el monto depositado*/
         printf("Tu saldo actual es de $%.2f", saldoActual);/*Mostramos solo dos decimales*/
         break;
+    case 2:
+        /*1.Le preguntamos al usuario cuanto quiere extraer*/
+        printf("Cuanto dinero desea extraer?\n");
+        /*2.Guardamos el monto en la variable extraerDinero*/
+        scanf("%f", &extraerDinero);
+        /*3.Hacemos un condicional if para el caso en el que el usuario quiera extraer mas dinero del que tiene en la cuenta*/
+        if(extraerDinero > saldoActual){
+            /*Si el monto a extraer es mayor al saldo en la cuenta ejecutamos el siguiente printf*/
+            printf("Ustede no cuenta con el dinero suficiente\n");
+        }
+        else{
+            /*En caso de que el monto a extraer sea menor que el saldo actual efectuamos la resta de los valores y mostramos en pantalla el ersultado*/
+            saldoActual -= extraerDinero;
+            printf("Su saldo actual es $%.2f", saldoActual);
+        }
+        break;
+    case 3:
+        /*En caso de que el ususario digite la opcion 3 ejecutamos el siguiente printf*/
+        printf("Usuario deslogueado\n");
+        break;
+    default: 
+        /*Si el usuario ingresa un valor distinto a los requeridos ejecutaremos el siguinte printf*/
+        printf("No ha digitado una opcion correcta");
+        break;
     }
+
+
+
     return 0;
 }
