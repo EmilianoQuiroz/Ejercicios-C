e3d1e88c4bc435bcf688554db6ee096b819edea2
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-12-04 17:12:30 -0300

    Ejercicio raiz cuadrada de un numero

---
+++ 01-EjerciciosDeEstructurasDeSeleccion/16-raiz_cuadrada.c
@@ -0,0 +1,21 @@
+//Raiz cuadrada
+/*Pedirle al usuario por pantalla que digite un numero y con el calcular su raiz cuadrada*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float numero,raizcuadrada;
+
+    printf("Digite un numero:\n");
+    scanf("%f", &numero);
+
+    raizcuadrada = sqrt(numero);
+
+    printf("La raiz cuadrada es %.2f", raizcuadrada);
+
+
+
+    return 0;
+}
