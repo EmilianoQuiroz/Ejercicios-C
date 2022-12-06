1a9285d1dc6b8f3e53aca359d76de0eaedaef9b2
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-12-06 20:10:06 -0300

    Ejercicio datos personales

--- 01-EjerciciosDeEstructurasDeSeleccion/17-datos_personales.c
+++ 01-EjerciciosDeEstructurasDeSeleccion/17-datos_personales.c
@@ -5,3 +5,28 @@ En caso de que el sexo sea femenino y la edad sea mayor 18 imprimir el siguiente
 En caso de que el sexo sea masculino, no importa la edad, imprimir el siguiente mensaje
 "Usuario denegado"
 */
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    char sexo[20];
+    int edad;
+
+    printf("Digite su sexo: \n");
+    fgets(sexo, 20, stdin);
+    strtok(sexo, "\n");
+
+    printf("Digite su edad: \n");
+    scanf("%d", &edad);
+
+    if((strcmp(sexo, "femenino")==0) && edad >= 18){
+        printf("Usuario aprobado");
+    }else{
+        printf("Usuario denegado");
+    }
+
+
+    return 0;
+}
