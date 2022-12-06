430bb4afb9320047b96d6fe1866dc85bb3722c92
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-12-05 23:42:18 -0300

    Ejercicio datos personales

---
+++ 17-datos_personales.c
@@ -0,0 +1,7 @@
+/*
+Pedirle al usuario que digite su edad y sexo
+En caso de que el sexo sea femenino y la edad sea mayor 18 imprimir el siguiente mensaje
+"Usuario aprobado"
+En caso de que el sexo sea masculino, no importa la edad, imprimir el siguiente mensaje
+"Usuario denegado"
+*/
